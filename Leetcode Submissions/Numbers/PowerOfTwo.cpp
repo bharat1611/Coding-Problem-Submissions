f46e3e74a8db2a6de800db2ee83cc9e47161d0e5
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-03-11 21:52:06 +0530

    PowerOfTwo

---
+++ Leetcode Submissions/Numbers/PowerOfTwo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0)
+            return 0;
+        while(n %2 == 0)
+        {
+            n=n/2;
+        }
+        return n==1;
+        
+    }
+};
