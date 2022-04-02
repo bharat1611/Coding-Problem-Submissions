9c2f2a73e65f2e288e72ffa7a7b04ad8e8b17c08
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-04-02 18:49:14 +0530

    24_Swap_nodes_in_Pairs_Leetcode

---
+++ Leetcode Submissions/24_Swap_Nodes_in_Pairs.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ #include <bits/stdc++.h>
+ using namespace std;
+ 
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) 
+        {
+            return head;
+        }
+        
+        ListNode * prev = nullptr;
+        ListNode * curr = head;
+        ListNode * nextt = head->next;
+        
+        head = head->next;
+        
+        while (curr != NULL && nextt != NULL) 
+        {
+            if (prev != NULL)
+                    prev->next = nextt;
+            curr->next = nextt->next;
+            nextt->next = curr;
+            prev = curr;
+            curr = curr->next;
+            if (curr != NULL)
+                nextt = curr->next;
+        }
+        return head;
+    }
+};
