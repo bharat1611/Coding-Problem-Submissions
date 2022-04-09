717330c6410cf9d275fdde7cd1a5e2e49db61755
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-04-09 22:39:26 +0530

    66_plus_one_leetcode

---
+++ Leetcode Submissions/66_Plus_one.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) 
+    {
+        reverse(digits.begin(),digits.end());
+        int carry = 1;
+        
+        for(int i = 0 ; i < digits.size() ; i++)
+        {
+            int temp = digits[i];
+            digits[i] = (digits[i]+carry)%10;
+            carry = (temp + carry)/10;
+        }
+        
+        if(carry)
+            digits.push_back(carry);
+        
+        reverse(digits.begin(),digits.end());
+        return digits;
+    }
+};
