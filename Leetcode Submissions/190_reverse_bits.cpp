b504086d418f184c0f1209705114832a1a15d991
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-04-30 19:05:28 +0530

    190_reverse_bits

---
+++ Leetcode Submissions/190_reverse_bits.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) 
+    {
+        uint32_t ans;
+        for(int i = 0; i<32; i++)
+        {
+            ans <<= 1;
+            ans |= n&1;
+            n = (n >> 1);
+        }
+        return ans;
+    }
+};
