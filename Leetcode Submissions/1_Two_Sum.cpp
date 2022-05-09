e2be935bdfba43d12ec6d49df16e5af165bcb68e
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-05-09 14:10:06 +0530

    Two_Sum_Vector_Pair_Approach

--- Leetcode Submissions/1_Two_Sum.cpp
+++ Leetcode Submissions/1_Two_Sum.cpp
@@ -1,22 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
-        unordered_map<int,int> mp;
-        vector<int> ans;
-        for(int i= 0;i < nums.size();i++)
-        {   
-            if(mp.find(target - nums[i]) != mp.end())
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) 
+//     {
+//         unordered_map<int,int> mp;
+//         vector<int> ans;
+//         for(int i= 0;i < nums.size();i++)
+//         {   
+//             if(mp.find(target - nums[i]) != mp.end())
+//             {
+//                 ans.push_back(i);
+//                 ans.push_back(mp[target - nums[i]]);
+//                 return ans;
+//             }
+//             mp[nums[i]] = i;
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution{
+    public:
+        vector<int> twoSum(vector<int>&nums, int target)
+        {
+            vector<pair<int, int>> v;
+            for(int i = 0 ; i < nums.size(); i++)
+                v.push_back(make_pair(nums[i], i));
+            sort(v.begin(), v.end());
+            vector<int>ans;
+            int i = 0, j = nums.size() - 1;
+            while(i < j)
             {
-                ans.push_back(i);
-                ans.push_back(mp[target - nums[i]]);
-                return ans;
+                if(v[i].first + v[j].first == target)
+                {
+                    ans.push_back(v[i].second);
+                    ans.push_back(v[j].second);
+                    break;
+                }
+                if(v[i].first + v[j].first > target)
+                    j--;
+                else
+                    i++;
             }
-            mp[nums[i]] = i;
+            return ans;
         }
-        return ans;
-    }
 };
