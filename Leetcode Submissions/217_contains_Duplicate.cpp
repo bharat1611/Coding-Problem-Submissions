75d8da150a9a9abada9291979c9df724638e2687
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-05-24 22:58:33 +0530

    217_contains_Duplicate

--- Leetcode Submissions/217_contains_Duplicate.cpp
+++ Leetcode Submissions/217_contains_Duplicate.cpp
@@ -11,3 +11,16 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>s;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(s.find(nums[i])!=s.end()) return true;
+            s.insert(nums[i]);
+        }
+        return false;
+    }   
+};
