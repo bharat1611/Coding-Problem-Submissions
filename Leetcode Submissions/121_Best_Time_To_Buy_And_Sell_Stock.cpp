33c350e5cf4671051b2df5b0158f1e518da0127a
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-05-31 17:02:47 +0530

    121_Best_Time_To_Buy_And_Sell_Stock

---
+++ Leetcode Submissions/121_Best_Time_To_Buy_And_Sell_Stock.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) 
+    {
+        int n = nums.size(), k = 0;
+        vector<int> v;
+        for(int i = 0 ; i < n - 1; i++)
+        {
+            k = i + 1;
+            while(k < nums.size() && nums[i] < nums[k])
+            {
+                v.push_back(nums[k] - nums[i]);
+                k++;
+            }
+        }
+        if(v.size() == 0)
+            return 0;
+        return *max_element(v.begin(), v.end());
+    }
+};
