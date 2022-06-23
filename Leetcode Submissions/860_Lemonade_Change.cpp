a9e14fce7aa6736a71b350c73e61fdb1bb8faee4
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-06-24 00:22:01 +0530

    860_lemonade_change

---
+++ Leetcode Submissions/860_Lemonade_Change.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        
+        int five = 0, ten = 0, twenty = 0;
+        for(int i = 0 ; i < bills.size(); i++)
+        {
+            if(bills[i] == 5)
+                five++;
+            else if(bills[i] == 10)
+            {
+                if(five == 0)
+                    return false;
+                
+                five--;
+                ten++;
+            }
+            else
+            {
+                if(five == 0)
+                    return false;
+                else if(ten >= 1 && five >= 1)
+                {
+                    five--;
+                    ten--;
+                }
+                else if(five >= 3)
+                    five -= 3;
+                else 
+                    return false;
+                
+            }
+                
+        }
+        return true;
+    }
+};
