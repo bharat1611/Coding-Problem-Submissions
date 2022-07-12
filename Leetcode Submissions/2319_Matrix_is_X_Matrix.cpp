56bbc44118991adfef43d76f292f13e73c532160
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-07-13 00:07:41 +0530

    2319_Matrix_Is_X_matrix

---
+++ Leetcode Submissions/2319_Matrix_is_X_Matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& a) {
+        int n = a.size();
+        
+        for(int i = 0 ; i < n; i++)
+        {
+            for(int j = 0 ; j < n; j++)
+            {
+                if(i == j || i + j == n - 1)
+                {
+                    if(a[i][j] == 0)
+                        return false;
+                }
+                else{
+                    if(a[i][j] != 0)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
