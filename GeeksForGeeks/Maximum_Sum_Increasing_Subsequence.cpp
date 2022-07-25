72227bd40231cf847f8332239e8af3466026a289
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-07-25 21:18:34 +0530

    Medium_Maximum Sum Increasing Subsequence

---
+++ GeeksForGeeks/Maximum_Sum_Increasing_Subsequence.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    int sum = 0, max_sum = 0;
+	    vector<int>dp(n);
+	    for(int i = 0 ;i < n; i++)
+	        dp[i] = arr[i];
+	        
+	    for(int i = 0 ; i < n; i++)
+	    {
+	        for(int j = 0 ; j < i ; j++)
+	        {
+	            if(arr[i] > arr[j])
+	            {
+	                dp[i] = max(arr[i] + dp[j], dp[i]);
+	            }
+	        }
+	    }
+	    return *max_element(dp.begin(), dp.end());
+	}  
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+      
+
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
