b1f570a8b839c6d5404942de02a55206e32ec702
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-07-25 21:14:12 +0530

    Easy_Missing Number in an Array

---
+++ GeeksForGeeks/Missing_Number_In_Array.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+ // } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& arr, int n) {
+        
+        int supposed = (n*(n + 1))/2;
+        int actual = 0 ;
+        for(int i = 0 ; i < arr.size(); i++)
+            actual += arr[i];
+        return (supposed-actual);
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
