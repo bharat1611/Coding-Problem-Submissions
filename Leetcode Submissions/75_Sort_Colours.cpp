9dc0fbecdb3f388a69baccd06d2cd9a5e1ccd763
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-08-11 19:44:09 +0530

    75_Sort_Colours - Solution 3

--- Leetcode Submissions/75_Sort_Colours.cpp
+++ Leetcode Submissions/75_Sort_Colours.cpp
@@ -38,3 +38,26 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int left = 0, right = nums.size() - 1;
+        int mid = 0;
+        while(mid <= right)
+        {
+            if(nums[mid] == 0){
+                swap(nums[left], nums[mid]);
+                mid++, left++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[right], nums[mid]);
+                right--;           
+            }
+        }    
+    }
+};
