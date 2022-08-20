38a0c642dab7f9dd24148e17deb8f069549f8f82
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-08-21 01:20:11 +0530

    1748_Sum_Of_Unique_Elements

---
+++ Leetcode Submissions/1748_Sum_Of_Unique_Elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            mp[nums[i]]++;
+        }
+        int sum = 0;
+        for(auto i : mp)
+        {
+            if(i.second == 1)
+                sum = sum + i.first;
+        }
+        return sum;
+    }
+};
