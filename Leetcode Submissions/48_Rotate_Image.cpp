2f068ae871fd69cd6c2187dae3cc9e0c1ca95563
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-08-21 20:04:15 +0530

    48_Rotate_Image

---
+++ Leetcode Submissions/48_Rotate_Image.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        for(int i = 0 ; i < matrix.size(); i++)
+        {
+            for(int j = 0 ; j < i; j++)
+            {
+               swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        // for(int i = 0 ; i < matrix.size(); i++)
+        // {
+        //     for(int j = 0 ; j <matrix[i].size(); j++)
+        //     {
+        //         cout << matrix[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        for(int i = 0 ; i < matrix.size(); i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+    
+};
