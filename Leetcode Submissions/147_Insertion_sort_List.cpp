78afcaf47cbda1e69cde6cb9681eb84b3746221e
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-08-23 20:23:47 +0530

    147_Insertion_Sort_Solution 2

--- Leetcode Submissions/147_Insertion_sort_List.cpp
+++ Leetcode Submissions/147_Insertion_sort_List.cpp
@@ -19,3 +19,24 @@ public:
         return l2 -> next;
     }
 };
+
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode *nxt = head -> next;
+        while(nxt != NULL)
+        {
+            ListNode *curr = head;
+            while(curr != nxt)
+            {
+                if(nxt -> val < curr -> val)
+                    swap(curr -> val, nxt -> val);
+                
+                curr = curr -> next;
+            }
+            nxt = nxt -> next;   
+        }
+        return head;
+    }
+};
+    
