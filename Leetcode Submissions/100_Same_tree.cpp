cdf1acdbb15c9395b4ed376dbadcec9cc28533bf
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-08-23 12:37:01 +0530

    100_Same_Tree

---
+++ Leetcode Submissions/100_Same_tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p != NULL && q != NULL)
+        {
+            if(p -> val == q -> val)
+                return (isSameTree(p -> left, q -> left) && isSameTree(p -> right , q -> right));
+        }
+        else if(p == NULL && q == NULL)
+            return true;
+        
+        return false;
+    }
+};
