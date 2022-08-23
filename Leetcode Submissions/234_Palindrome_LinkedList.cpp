477e82704d4c1e88588a7da638ababba1f8f03f2
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-08-23 08:36:21 +0530

    234_Palindrome_Linked_List

--- Leetcode Submissions/234_Palindrome_LinkedList.cpp
+++ Leetcode Submissions/234_Palindrome_LinkedList.cpp
@@ -16,3 +16,48 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        ListNode *l2 = new ListNode(0);
+        ListNode *temp = l2;
+        ListNode *t1 = head;
+        while(t1)
+        {
+            temp -> next = new ListNode(t1 -> val);
+            temp = temp -> next;
+            t1 = t1 -> next;
+        }
+        l2 = l2 -> next;            //Creating a copy of original LL
+        ListNode *nxt = NULL, *curr = head, *prev = NULL;
+        while(curr != NULL)         // Reversing the Linked List
+        {
+            nxt = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        while(prev && l2)           // Checking if both have the same values
+        {
+            if(l2 -> val != prev -> val)
+                return false;
+            l2 = l2 -> next;
+            prev = prev -> next;
+        }
+//         while(prev != NULL)
+//         {
+//             cout << prev -> val <<  " ";
+//             prev = prev -> next; 
+//         }
+//         cout << endl;
+//         while(l2 != NULL)
+//         {
+//             cout << l2 -> val << " " ;
+//             l2 = l2 -> next;
+//         }
+        return true;
+    }
+};
