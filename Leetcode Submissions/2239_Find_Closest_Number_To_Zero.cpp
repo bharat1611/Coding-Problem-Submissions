4fe7fae182861fe5e50cf8a7200a805bb16bf5fe
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-08-26 15:39:54 +0530

    2239. Find Closest Number to Zero

--- Leetcode Submissions/2239_Find_Closest_Number_To_Zero.cpp
+++ Leetcode Submissions/2239_Find_Closest_Number_To_Zero.cpp
@@ -18,3 +18,32 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        
+        int min_p = INT_MAX, min_n = INT_MIN;
+        
+        for(int i = 0 ; i < nums.size();i++)
+        {
+            if(nums[i] > 0)
+            {
+                if(nums[i] < min_p)
+                    min_p = nums[i];
+            }
+            else
+            {
+                if(nums[i] > min_n)
+                    min_n = nums[i];
+            }
+        }
+        if(min_p == INT_MAX)
+            return min_n;
+        if(min_n == INT_MIN)
+            return min_p;
+        
+        return abs(min_p) <= abs(min_n) ? min_p : min_n;
+    }
+};
