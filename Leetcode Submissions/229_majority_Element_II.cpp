4d7ffc4affb5ec759eabcc9ffad3ed7a7a3f860a
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-08-29 13:37:03 +0530

    229_Majority-elementII

---
+++ Leetcode Submissions/229_majority_Element_II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        map<int, int> mp;
+        
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            mp[nums[i]]++;
+        }
+        vector<int> ans;
+        
+        for(auto i : mp)
+        {
+            if(i.second > n/3)
+                ans.push_back(i.first);
+        }
+        return ans;
+    }
+};
