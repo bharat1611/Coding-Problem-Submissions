725790c908e18351caf7835a7b68b56d42275d2b
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-02 12:55:07 +0530

    102_Binary_Tree_level_Order_Traversal

---
+++ Leetcode Submissions/102_Binary_Tree_level_Order_Traversal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode *> q;
+        if(root == NULL)
+            return ans;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            vector<int> level;
+            for(int i = 0 ; i < n; i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node -> left)
+                    q.push(node -> left);
+                if(node -> right)
+                    q.push(node -> right);
+                level.push_back(node -> val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
