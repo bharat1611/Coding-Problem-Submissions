b7a3291909926a051ad4baa44743d75531e4ac74
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-02 21:57:28 +0530

    513_Find_Bottom_Left_Tree_Value

--- Leetcode Submissions/513_Find_Bottom_Left_Tree_Value.cpp
+++ Leetcode Submissions/513_Find_Bottom_Left_Tree_Value.cpp
@@ -26,3 +26,31 @@ public:
         return v[n - 1][0];  
     }
 };
+
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> ans;
+        while(!q.empty())
+        {
+            ans.clear();
+            int n = q.size();
+            for(int i = 0 ; i < n ; i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                if(node -> left)
+                    q.push(node -> left);
+                if(node -> right)
+                    q.push(node -> right);
+                ans.push_back(node -> val);
+            }
+        }
+        return ans[0];
+    }
+};
