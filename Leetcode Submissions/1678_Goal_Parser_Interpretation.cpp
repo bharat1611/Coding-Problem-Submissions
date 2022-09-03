f833552b6c12d21b4cd27799c6013673b23b1d9f
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-03 21:22:36 +0530

    1678_Goal_Interpretation

---
+++ Leetcode Submissions/1678_Goal_Parser_Interpretation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string interpret(string command) {
+
+        string ans = "";
+        for(int i = 0 ; i < command.length(); i++)
+        {
+            if(command[i] == 'G')
+            {
+                ans += 'G';
+            }
+            else if(command[i] == '(' and command[i + 1] == ')')
+            {
+                ans += 'o';
+                i++;
+            }
+            else if(command[i] == '(' and command[i + 1] == 'a')
+            {
+                ans += "al";
+                i++;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
