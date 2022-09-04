8750a77cbaad666801e9e80e8f7b57552ae896e0
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-04 23:32:55 +0530

    2351_First Letter To Appear Twice

---
+++ Leetcode Submissions/2351_First_Letter_To_Appear_Twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        map<char, int> mp;
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(mp.find(s[i]) != mp.end())
+                return s[i];
+            mp[s[i]]++;
+        }
+        return ' ';
+    }
+};
