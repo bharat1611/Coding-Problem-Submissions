3ada63023298ddaf2dfc08ce682217b4fb4a5b03
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-07 12:06:56 +0530

    1845_Seat_reservation_manager - Solution 2

--- Leetcode Submissions/1845_Seat_Reservation_Manager.cpp
+++ Leetcode Submissions/1845_Seat_Reservation_Manager.cpp
@@ -16,3 +16,22 @@ public:
         s.insert(seatNumber);        
     }
 };
+
+class SeatManager {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    SeatManager(int n) {
+        for(int i = 1; i <= n; i++)
+            pq.push(i);
+    }
+    
+    int reserve() {
+        int a = pq.top();
+        pq.pop();
+        return a;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);        
+    }
+};
