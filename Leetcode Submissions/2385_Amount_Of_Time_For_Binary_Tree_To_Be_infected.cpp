46246ef0595c97860d65a81c93acad3d678ce984
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-10 17:29:35 +0530

    2385_Amount_Of_Time_For_Binary_Tree_To_Be_infected

--- Leetcode Submissions/2385_Amount_Of_Time_For_Binary_Tree_To_Be_infected.cpp
+++ Leetcode Submissions/2385_Amount_Of_Time_For_Binary_Tree_To_Be_infected.cpp
@@ -1,6 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int ans = 0;
+    // Function to build the graph of the given tree
     void buildGraph(TreeNode *root, vector<vector<int>> &adj)
     {
         if(root == NULL)
@@ -18,33 +30,41 @@ public:
         buildGraph(root -> left, adj);
         buildGraph(root -> right, adj);
     }
+    // function to implement bfs in the graph
+    
     void bfs(vector<vector<int>> &adj, vector<bool> &visited, int start)
     {
         queue<int> q;
+        // We start the BFS from start node and traverse the entire graph
+        // The time it takes to traverse is also the time it takes to get infected
         q.push(start);
         while(!q.empty())
         {
             int n = q.size();
             for(int i = 0 ; i < n; i++)
             {
-                int u = q.front();
+                int u = q.front();      // Extracting the node that needs to be traversed
                 q.pop();
-                visited[u] = true;
+                visited[u] = true;         // marking the currently traversed node to true
                 for(int i = 0; i < adj[u].size(); i++)
                 {
-                    if(!visited[adj[u][i]])
+                     // if adjacent nodes are not visited, push them into the queue
+                    if(!visited[adj[u][i]]) 
                         q.push(adj[u][i]);
                 }
             }
-            ans++;
-        }
+            ans++;       // keep a count for the traversal
+        }   
     }
     int amountOfTime(TreeNode* root, int start) {
+        // vector to store the graph
         vector<vector<int>> adj(100001);
+        // vector to check which nodes are visited
         vector<bool> visited(100001, false);
         
         buildGraph(root, adj);
         bfs(adj, visited, start);
+        // return ans - 1, coz the start node is also counted in traversal, but it was already infeced
         return ans - 1;
     }
 };
