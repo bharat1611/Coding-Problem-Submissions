24c3c6c5e75c384c62f672ddd46349800a7eeb0f
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-13 00:31:00 +0530

    36_Valid_Sudoku

--- Leetcode Submissions/36_Valid_Sudoku.cpp
+++ Leetcode Submissions/36_Valid_Sudoku.cpp
@@ -1,26 +1,30 @@
-class Solution {
+class Solution{
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
         unordered_map<int, unordered_set<int>> rows;
         unordered_map<int, unordered_set<int>> col;
         map<pair<int, int>, unordered_set<int>> box;
-        
-        for(int i = 0 ; i < 9; i++)
+
+        for (int i = 0; i < 9; i++)
         {
-            for(int j = 0 ; j < 9; j++)
+            for (int j = 0; j < 9; j++)
             {
-                if(board[i][j] == '.')
+                if (board[i][j] == '.')
                     continue;
-                
-                if(rows[i].find(board[i][j]) != rows[i].end() || col[i].find(board[i][j]) != col[i].end() || box[{i/3,j/3}].find(board[i][j]) != box[{i/3,j/3}].end())
-                   return false;
+
+                if (rows[i].find(board[i][j]) != rows[i].end() or
+                    col[j].find(board[i][j]) != col[j].end() or
+                    box[{i/3, j/3}].find(board[i][j]) != box[{i/3, j/3}].end())
+                {
+                    return false;
+                }
                 else
-                   {
-                        rows[i].insert(board[i][j]);
-                        col[j].insert(board[i][j]);
-                        box[{i/3,j/3}].insert(board[i][j]);
-                   }      
+                {
+                    rows[i].insert(board[i][j]);
+                    col[j].insert(board[i][j]);
+                    box[{i/3, j/3}].insert(board[i][j]);
+                }
             }
         }
         return true;
