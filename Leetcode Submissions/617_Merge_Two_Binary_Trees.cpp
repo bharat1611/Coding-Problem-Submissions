496d2213f1812d6c7665ae10a4f074e3665dd943
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-17 22:23:18 +0530

    617_Merge_Two_Binary_Trees

---
+++ Leetcode Submissions/617_Merge_Two_Binary_Trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        
+        if(root1 == NULL and root2 == NULL)
+            return NULL;
+        if(!root2)
+            return root1;
+        if(!root1)
+            return root2;
+        root1 -> val += root2 -> val;
+        root1 -> left = mergeTrees(root1 -> left, root2 -> left);
+        root1 -> right = mergeTrees(root1 -> right, root2 -> right);
+        return root1;
+    }
+};
