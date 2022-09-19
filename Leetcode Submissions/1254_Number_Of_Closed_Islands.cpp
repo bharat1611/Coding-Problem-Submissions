a8dc7970b78ac98c174bedfb612118d277e1d4ae
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-19 21:08:31 +0530

    1254_Number_Of_Closed_Islands

--- Leetcode Submissions/1254_Number_Of_Closed_Islands.cpp
+++ Leetcode Submissions/1254_Number_Of_Closed_Islands.cpp
@@ -18,16 +18,8 @@ public:
             for(int j = 0 ; j < grid[0].size(); j++)
             {
                 if((i == 0 || j == 0 || i == grid.size() - 1 || j == grid[0].size() - 1 ) and grid[i][j] == 0)
-                    grid[i][j] == 1;
+                    helper(grid, i, j);
             }
-        }
-        for(int i = 0; i < grid.size(); i++)
-        {
-            for(int j = 0 ; j < grid[0].size(); j++)
-            {
-                cout << grid[i][j] << " ";
-            }
-            cout << endl;
         }
         for(int i = 0 ; i < grid.size() - 1; i++)
         {
