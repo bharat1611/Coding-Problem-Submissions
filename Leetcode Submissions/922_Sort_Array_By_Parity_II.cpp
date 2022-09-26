80fa972b000872fd7fabd5cecf3a1516dd699460
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-27 02:07:50 +0530

    922_Sort_Array_By_Parity_II_SOlution 2

--- Leetcode Submissions/922_Sort_Array_By_Parity_II.cpp
+++ Leetcode Submissions/922_Sort_Array_By_Parity_II.cpp
@@ -26,3 +26,25 @@ public:
         return nums;
     }
 };
+
+class Solution
+{
+public:
+    vector<int> sortArrayByParityII(vector<int> &nums)
+    {
+        int i = 0, j = 1, n = nums.size();
+        while (i < n && j < n)
+        {
+            if (nums[i] % 2 == 0)
+                i += 2;
+            else if (nums[j] % 2 != 0)
+                j += 2;
+            else
+            {
+                swap(nums[i], nums[j]);
+                i += 2;
+                j += 2;
+            }
+        }
+        return nums;
+    }
