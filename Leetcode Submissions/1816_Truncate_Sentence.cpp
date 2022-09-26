839178cb598525bd5822ab4c006ad8faf5e55e6b
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-26 21:31:23 +0530

    1816_Truncate_Sentence

---
+++ Leetcode Submissions/1816_Truncate_Sentence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int count = 0;
+        string temp = "", ans = "";
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(s[i] == ' ')
+            {
+                count++;
+                if(count == k){
+                    ans += temp;
+                    return ans;
+                }
+                temp += ' ';
+                ans += temp;
+                temp = "";
+                
+            }
+            else
+                temp += s[i];
+        }
+        ans += temp;
+        return ans;
+    }
+};
