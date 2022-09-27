baf6922627528d16987a6d817285c550afd90074
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-27 21:46:13 +0530

    926_Flip_String_To_Monotone_Increasing

---
+++ Leetcode Submissions/926_Flip_String_To_Monotone_Increasing.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        vector<int> dp(s.length(), 0);
+        int temp = 0, ans = INT_MAX;
+        // left to right, counting 1's
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(s[i] == '1')
+                temp += 1;
+            dp[i] = temp;
+        }
+        temp = 0;
+        // right to left, counting 0's
+        for(int i = s.length() - 1; i >= 0; i--)
+        {
+            if(s[i] == '0')
+                temp += 1;
+            dp[i] += temp;
+            ans = min(ans, dp[i]);
+        }
+        return ans - 1;
+    }
+};
