f11053c221d027410bef3dc460ed509fa368bbba
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-09-30 00:20:14 +0530

    2335_Minimum_Amount_Of_time_to_Fill_Cups

--- Leetcode Submissions/2335_Minimum_Amount_Of_time_to_Fill_Cups.cpp
+++ Leetcode Submissions/2335_Minimum_Amount_Of_time_to_Fill_Cups.cpp
@@ -25,3 +25,34 @@ public:
         return count;
     }
 };
+class Solution {
+public:
+    int fillCups(vector<int>& amount) {
+        priority_queue<int> pq;
+        
+        for(int i = 0; i < amount.size(); i++)
+            pq.push(amount[i]);
+        int count = 0;
+        while(!pq.empty())
+        {
+            
+            if(pq.size() > 2 and pq.top() != 0){
+                int a = pq.top();
+                pq.pop();
+                int b = pq.top();
+                pq.pop();
+                a--;b--;
+                pq.push(a);
+                pq.push(b);
+                count++;
+            }
+            else{
+                count = count + pq.top();
+                break;
+            }
+            
+            
+        }
+        return count;
+    }
+};
