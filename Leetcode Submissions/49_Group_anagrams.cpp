55911eb3ff4542bb580e5f0cfb048685e235139d
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-10-28 21:48:36 +0530

    49_Group_Anagrams

---
+++ Leetcode Submissions/49_Group_anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        unordered_map<string, vector<string>> mp;
+        int n = strs.size();
+        string temp;
+
+        for (int i = 0; i < n; ++i)
+        {
+            temp = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            mp[strs[i]].push_back(temp);
+        }
+
+        vector<vector<string>> result;
+
+        for (auto i : mp)
+            result.push_back(i.second);
+
+        return result;
+    }
+};
