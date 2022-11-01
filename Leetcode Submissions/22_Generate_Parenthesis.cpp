bc2ef08107da48ddb674180f2b001d375eb4e9be
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-02 00:14:36 +0530

    22_Generate_Parenthesis = Solution 2

--- Leetcode Submissions/22_Generate_Parenthesis.cpp
+++ Leetcode Submissions/22_Generate_Parenthesis.cpp
@@ -21,3 +21,35 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    vector<string> ans;
+    void generate(string s, int open, int close, int n)
+    {
+        if(open == n and close == n)
+        {
+            ans.push_back(s);
+            return;
+        }
+        if(open < n)
+        {
+            s = s + "(";
+            generate(s, open + 1, close, n);
+            s.pop_back();
+        }
+        if(close < open)
+        {
+            s = s + ")";
+            generate(s, open, close + 1, n);
+            s.pop_back();
+        }
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        {
+            generate("", 0, 0, n);
+            return ans;
+        }
+    }
+};
