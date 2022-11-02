c680952b960c09fd1182bfb0c2f2fcddbf3c5705
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-02 15:02:52 +0530

    433_Minimum_Genetic_Mutation

--- Leetcode Submissions/433_Minimum_Genetic_Mutation.cpp
+++ Leetcode Submissions/433_Minimum_Genetic_Mutation.cpp
@@ -9,10 +9,11 @@ public:
         // if end is not present in bank then return -1;
         if(st.count(end) == 0)
             return -1;
-        
+        // initial count = 1;
         int count = 1;
-        
+        // vector of all the possible mutations
         vector<char> mut_opt = {'A', 'C', 'G', 'T'};
+        // Actual Iterative BFS
         queue<string> q;
         q.push(start);
         
@@ -21,30 +22,39 @@ public:
             int n = q.size();
             for(int j = 0 ; j < n; j++)
             {
+                // extracting front of queue
                 string s = q.front();
                 q.pop();
+                // exploring the current string
                 for(int i = 0 ; i < s.size(); i++)
                 {
+                    // storing the original character 
                     char org = s[i];
-                    
+                    // trying to mutate the current string with possibilities
                     for(int k = 0 ; k < mut_opt.size(); k++)
                     {
+                        // if current character is not equal to thhe mutation
                         if(org != mut_opt[k])
                             s[i] = mut_opt[k];
-                        
+                        // if after mutation the string is equal to end result
                         if(s == end)
                             return count;
+                        // if not, then check if the string is present in the bank
                         if(st.count(s) > 0)
                         {
+                            // push the current string and erase it, coz 1 string can be used only once
                             q.push(s);
                             st.erase(s);
                         }
                     }
+                    // replacing the changed character by org character
                     s[i] = org;
                 }
             }
+            // increment count by each iteration
             count++;
         }
+        // if not possible, return -1
         return -1;
         
     }
