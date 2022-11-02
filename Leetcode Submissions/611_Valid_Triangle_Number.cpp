7b3a04dcf63a63479e60a04653ad1408640c5b97
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-02 23:27:25 +0530

    611_Valid_Triangle_Number

---
+++ Leetcode Submissions/611_Valid_Triangle_Number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int count = 0;
+        for(int k = nums.size() - 1; k > 1; k--)
+        {
+            int j = k - 1;
+            int i = 0;
+            while(i < j)
+            {
+                if(nums[i] + nums[j] > nums[k]){
+                    count += j - i;
+                    j--;
+                }
+                else
+                    i++;
+            }
+            
+        }
+        return count;   
+    }
+};
