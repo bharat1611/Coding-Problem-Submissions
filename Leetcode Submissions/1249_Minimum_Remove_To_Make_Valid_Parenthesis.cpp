3287ea0c959578d9a20ba310b8680ec661479d36
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-03 14:32:18 +0530

    1249_Minimum_Remove_To_Make_Valid_Parenthesis - Solution 2

--- Leetcode Submissions/1249_Minimum_Remove_To_Make_Valid_Parenthesis.cpp
+++ Leetcode Submissions/1249_Minimum_Remove_To_Make_Valid_Parenthesis.cpp
@@ -38,3 +38,34 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(s[i] == '(')
+                st.push(i);
+            else if(s[i] == ')')
+            {
+                if(!st.empty())
+                    st.pop();
+                else
+                    s[i] = '#';               
+            }
+        }
+        while(!st.empty())
+        {
+            s[st.top()] = '#';
+            st.pop();
+        }
+        string ans = "";
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(s[i] != '#')
+                ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
