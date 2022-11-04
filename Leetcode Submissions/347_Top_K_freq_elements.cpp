00e862a084387b1c751bdeb2ae002741b9bdfbe5
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-04 11:21:48 +0530

    347_Top_K_Freq_Elements

--- Leetcode Submissions/347_Top_K_freq_elements.cpp
+++ Leetcode Submissions/347_Top_K_freq_elements.cpp
@@ -23,3 +23,25 @@ public:
         return {};
     }
 };
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for(int i = 0 ; i < nums.size(); i++)
+            mp[nums[i]]++;
+        
+        vector<pair<int, int>> v;
+        for(auto i : mp)
+        {
+            v.push_back({i.second, i.first});
+        }
+        sort(v.rbegin(), v.rend());
+        vector<int> ans;
+        for(int i = 0 ; i < k; i++)
+        {
+            ans.push_back(v[i].second);
+        }
+        return ans;
+    }
+};
