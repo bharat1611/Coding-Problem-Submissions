82158672520f8fa2acbaae50709a7829db5fe8f6
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-06 12:31:03 +0530

    899_Orderly_queue

--- Leetcode Submissions/899_Orderly_Queue.cpp
+++ Leetcode Submissions/899_Orderly_Queue.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     string orderlyQueue(string s, int k) {
-        
+        // if k == 1 then we rotate the string and find the lexicographically minimum string
         if(k == 1)
         {
             string ans = s;
             int n = s.length();
             for(int i = 0 ; i < n;i++)
             {
+                // extracting end char and adding it to the front
                 char c = s[n - 1];
                 s.pop_back();
                 s = c + s;
+                // minimum of existing string and new string formed
                 ans = min(s, ans);   
             }
             return ans;
         }
+        // if k > 1 then we can rearrange the string anyway we like
+        // so return the sorted string.
         sort(s.begin(), s.end());
         return s;
     }
