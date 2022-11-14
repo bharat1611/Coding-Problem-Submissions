903d134db0f94b393676cc85740f618c75c6d2a7
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-14 13:09:22 +0530

    947_Most_Stones_Removed_With_Same_Row_or_Column - Solution 2

--- Leetcode Submissions/947_Most_Stones_Removed_With_Same_Row_or_Column.cpp
+++ Leetcode Submissions/947_Most_Stones_Removed_With_Same_Row_or_Column.cpp
@@ -23,3 +23,37 @@ public:
         return n - componentSize;
     }
 };
+
+class Solution {
+public:
+    bool isValid(vector<int>&a, vector<int>&b)
+    {
+        if(a[0] == b[0])
+            return true;
+        if(a[1] == b[1])
+            return true;
+        return false;
+    }
+    void dfs(vector<vector<int>> &stones, vector<bool>&visited, int ind)
+    {
+        visited[ind] = true;
+        for(int j = 0 ; j < stones.size(); j++)
+        {
+            if(visited[j] == false and isValid(stones[j], stones[ind]))
+                dfs(stones, visited, j);
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        vector<bool> visited(n, false);
+        int componentSize = 0;
+        for(int i = 0 ; i < n; i++)
+        {
+            if(visited[i] == true)
+                continue;
+            dfs(stones, visited, i);
+            componentSize++;
+        }
+        return n - componentSize;
+    }
+};
