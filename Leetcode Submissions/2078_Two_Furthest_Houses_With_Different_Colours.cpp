7cab18b7510332c46cbbbce4a09c954cd7f21641
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-17 20:46:56 +0530

    2078_Two_Furthest_Houses_With_Different_Colours - solution2

--- Leetcode Submissions/2078_Two_Furthest_Houses_With_Different_Colours.cpp
+++ Leetcode Submissions/2078_Two_Furthest_Houses_With_Different_Colours.cpp
@@ -16,3 +16,17 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int left = 0, right = n - 1;
+        while(colors[left] == colors[n - 1])
+            left++;
+        while(colors[right] == colors[0])
+            right--;
+        return max(right, n - 1 - left);
+            
+    }
+};
