c4b31c1be368ebc3e024ad11d49206b169a56554
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-20 15:29:00 +0530

    405_Convert_A_Number_To_HexaDecimal_number

---
+++ Leetcode Submissions/405_Convert_A_Number_To_HexaDecimal_number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if(num == 0)
+            return "0";
+        string ans = "";
+        string conv = "0123456789abcdef";
+        unsigned int n = num;
+        while(n)
+        {
+            int carry = n%16;
+            if(carry >= 10)
+                ans += conv[carry];
+            else
+                ans += to_string(carry);
+            n /= 16;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
