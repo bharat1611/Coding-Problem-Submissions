a3dd9f0b413be9224edb0e0131046a576b11c039
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-23 15:18:14 +0530

    Validate_BST - Both Methods

--- G1/2022_11_22_validateBST.cpp
+++ G1/2022_11_22_validateBST.cpp
@@ -1,8 +1,6 @@
 // Given a Binary Tree, validate whether this Binary Tree is a Binary Search Tree.
 
-// A BST is a bianry tree where for every node, each value on the left of the current node is less than itself, and on right of current node is greater than its value.
-
-// The easy approach to this problem is to do an inorder traversal of the tree and store it in an array. If that inorder traversal array is sorted, then the tree is a BST otherwise it is not a BST.
+// A BST is a binary tree where for every node, each value on the left of the current node is less than itself, and on right of current node is greater than its value.
 
 #include <iostream>
 #include <climits>
@@ -18,6 +16,23 @@ TreeNode *newN(int data)
     node -> left = node -> right = NULL;
     return (node);
 }
+
+// The easy approach to this problem is to do an inorder traversal of the tree and store it in an array. If that inorder traversal array is sorted, then the tree is a BST otherwise it is not a BST.
+
+// class Solution1{
+//     public :
+//     vector<int> v;
+//     void inorder(TreeNode *root)
+//     {
+//         if(root == NULL)
+//             return;
+//         inorder(root -> left);
+//         v.push_back(root -> val);
+//         inorder(root -> right);
+//     }
+// }
+
+// Another approach would be to check if nodes on left of the current node are less than current, and on the right are more than the current node.
 
 class Solution{
     public:
@@ -44,6 +59,19 @@ int main()
     root -> right = newN(8);
     root -> right -> left = newN(7);
     root -> right -> right = newN(10);
+    // Solution1 
+    // Solution1 obj1;
+    // obj.inorder(root);
+    // cout << "This tree is a BST : " ;
+    // for(int i = 0 ; i < v.size() - 1; i++)
+    // {
+    //     if(v[i] > v[i + 1]){
+    //         cout << "False";
+    //         break;
+    //     }
+    // }
+    // cout << "True ";
+
     Solution obj;
     cout << "This tree is a BST : " ;
     obj.isBST(root) == true ? cout << "True " : cout << "False";
