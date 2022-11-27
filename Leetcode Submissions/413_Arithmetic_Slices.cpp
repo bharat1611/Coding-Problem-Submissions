6e355df8c0963c9a3ed2e4e8f228c2ce41979520
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-27 14:32:34 +0530

    413_Arithmetic_Slices - SOlution 2

--- Leetcode Submissions/413_Arithmetic_Slices.cpp
+++ Leetcode Submissions/413_Arithmetic_Slices.cpp
@@ -1,3 +1,4 @@
+// Recursion - 0ms
 class Solution {
 public:
     int solve(vector<int> &nums, int i, int diff)
@@ -19,3 +20,47 @@ public:
         return ans;
     }
 };
+
+// DP + Memoisation 
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if(nums.size() < 3)
+            return 0;
+        vector<int> dp(nums.size(), 0);
+        int ans = 0;
+        for(int i = 2 ; i < nums.size(); i++)
+        {
+            if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2])
+            {
+                dp[i] = 1 + dp[i - 1];
+                ans += dp[i];
+            }
+        }
+        return ans;
+    }
+};
+
+// DP + Tabulation
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if(nums.size() < 3)
+            return 0;
+        int ans = 0;
+        int curr= 0;
+        for(int i = 2 ; i < nums.size(); i++)
+        {
+            if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2])
+            {
+                curr++;
+                ans += curr;
+            }
+            else
+                curr = 0;
+        }
+        return ans;
+    }
+};
+
+
