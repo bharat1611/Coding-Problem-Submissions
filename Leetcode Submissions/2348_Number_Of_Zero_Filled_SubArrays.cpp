584a6f1ae9aaf6cd0ac15349f33072d1d32d81b1
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-27 18:30:38 +0530

    2348_Number_Of_Zero_Filled_SubArrays

---
+++ Leetcode Submissions/2348_Number_Of_Zero_Filled_SubArrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long count = 0;
+        long long ans = 0;
+        
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            if(nums[i] == 0)
+                count++;
+            else{
+                ans += ((count)*(count + 1))/2;
+                count = 0;
+            }
+        }
+        if(count > 0)
+            ans += ((count)*(count + 1))/2;
+            
+        return ans;   
+    }
+};
