1e2fd56ffe105831cb6cf7eef829ab77af1cf8ef
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-11-28 20:51:23 +0530

    Permutations_II

--- Leetcode Submissions/Permutations_II.cpp
+++ Leetcode Submissions/Permutations_II.cpp
@@ -1,6 +1,6 @@
-class Solution {
+class Solution{
 public:
-    vector<vector<int>>ans;
+    vector<vector<int>> ans;
     set<vector<int>> st;
     // void print(vector<int>nums)
     // {
@@ -8,35 +8,36 @@ public:
     //         cout<<nums[i]<<" ";
     //     cout<<endl;
     // }
-    
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        if(nums.size()==1)
+
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        if (nums.size() == 1)
         {
             ans.push_back(nums);
             return ans;
         }
-        int index=0;
-        solve(nums,index);
-        for(auto i : st)
+        int index = 0;
+        solve(nums, index);
+        for (auto i : st)
             ans.push_back(i);
         return ans;
     }
-    void solve(vector<int>& nums, int index)
+    void solve(vector<int> &nums, int index)
     {
-        if(index>=nums.size())
+        if (index >= nums.size())
         {
-            if(st.find(nums) == st.end())
+            if (st.find(nums) == st.end())
                 st.insert(nums);
             return;
         }
-        solve(nums, index+1);
-        for(int i=index+1; i<nums.size(); i++)
+        solve(nums, index + 1);
+        for (int i = index + 1; i < nums.size(); i++)
         {
-            if(nums[i]!=nums[index])
+            if (nums[i] != nums[index])
             {
                 swap(nums[index], nums[i]);
-                solve(nums,index+1);
-                swap(nums[index], nums[i]);  
+                solve(nums, index + 1);
+                swap(nums[index], nums[i]);
             }
         }
     }
