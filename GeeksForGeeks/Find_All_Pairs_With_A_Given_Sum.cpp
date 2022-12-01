dd24893386a2de3063a1fc761475ac436736cc94
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-12-01 15:57:01 +0530

    Find_All_Pairs_With_A_Given_Sum

---
+++ GeeksForGeeks/Find_All_Pairs_With_A_Given_Sum.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int a[], int b[], int n, int m, int k)
+    {
+        unordered_map<int, int> mp;
+        for(int i = 0 ; i < m; i++)
+            mp[b[i]]++;
+        vector<pair<int, int>> ans;   
+        for(int i = 0 ; i < n; i++)
+        {
+            if(mp.find(k - a[i]) != mp.end())
+            {
+                ans.push_back({a[i], k - a[i]});
+                mp[k - a[i]]--;
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
+// } Driver Code Ends
