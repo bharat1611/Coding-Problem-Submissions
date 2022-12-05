a104918fcdd078b8660e6b092cb736f356c54fa7
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-12-05 22:26:21 +0530

    540_Single_element_In_A_Sorted_Array

--- Leetcode Submissions/540_Single_element_In_A_Sorted_Array.cpp
+++ Leetcode Submissions/540_Single_element_In_A_Sorted_Array.cpp
@@ -12,3 +12,16 @@ public:
         return 0;
     }
 };
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
