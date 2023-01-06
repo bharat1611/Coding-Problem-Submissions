d29f6fea4d1bf7096e417b018190175f86636949
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-01-06 15:48:09 +0530

    1833_Maximum_Ice_cream_Bars

---
+++ Leetcode Submissions/1833_Maximum_Ice_cream_Bars.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int count = 0;
+        for(int i = 0 ; i < costs.size(); i++)
+        {
+            if(coins - costs[i] < 0)
+                return count;
+            coins -= costs[i];
+            count++;
+        }
+        return count;
+    }
+};
