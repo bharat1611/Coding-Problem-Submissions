c5ac2b193529ebf2b5b3708caf7d831fe78bac44
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-03-01 13:54:23 +0530

    771_Jewels And Stones Solution 3

--- Leetcode Submissions/771_Jewels_And_Stones.cpp
+++ Leetcode Submissions/771_Jewels_And_Stones.cpp
@@ -15,22 +15,37 @@ public:
     }
 };
 
+// class Solution {
+// public:
+//     int numJewelsInStones(string jewels, string stones) {
+//         int count = 0 ;
+//         unordered_map<char, int> mp;
+//         for(int i = 0 ; i < stones.size(); i++)
+//         {
+//             mp[stones[i]]++;
+//         }
+//         for(int i = 0 ; i < jewels.size(); i++)
+//         {
+//             if(mp.find(jewels[i]) != mp.end())
+//             {
+//                 count += mp[jewels[i]];
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        int count = 0 ;
-        unordered_map<char, int> mp;
-        for(int i = 0 ; i < stones.size(); i++)
-        {
-            mp[stones[i]]++;
-        }
+        int c = 0 ;
         for(int i = 0 ; i < jewels.size(); i++)
         {
-            if(mp.find(jewels[i]) != mp.end())
-            {
-                count += mp[jewels[i]];
-            }
+            c += count(stones.begin(), stones.end(), jewels[i]);    
         }
-        return count;
+        return c;
     }
 };
+
+
+
