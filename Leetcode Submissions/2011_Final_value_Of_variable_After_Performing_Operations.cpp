ee20f168bf8f1e9d8ed0743f37f62b9d6f846a9a
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-03-01 13:47:19 +0530

    2011_Final Value og Variable after performing Operations

--- Leetcode Submissions/2011_Final_value_Of_variable_After_Performing_Operations.cpp
+++ Leetcode Submissions/2011_Final_value_Of_variable_After_Performing_Operations.cpp
@@ -21,3 +21,33 @@ public:
         return x;
     }
 };
+
+// class Solution {
+// public:
+//     int finalValueAfterOperations(vector<string>& operations) {
+//         int count = 0;
+//         for(int i = 0 ; i < operations.size(); i++)
+//         {
+//             if(operations[i] == "--X" or operations[i] == "X--")
+//                 count--;
+//             if(operations[i] == "++X" or operations[i] == "X++")
+//                 count++;
+//         }
+//         return count;
+//     }
+// };
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int p = 0, n = 0, total = 0;
+        for(int i = 0 ; i < operations.size(); i++)
+        {
+            p += count(operations[i].begin(), operations[i].end(), '+');
+            n += count(operations[i].begin(), operations[i].end(), '-');
+        }
+        total = p - n;
+        int num = total/2;
+        return num;
+        
+    }
+};
