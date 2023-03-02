a53bad9acf7a2d7431ada3f4f23c0cde47f3b8b2
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-03-02 11:05:45 +0530

    345_Reverse_Vowels_in_A_String

--- Leetcode Submissions/345_Reverse_Vowels_Of_A_String.cpp
+++ Leetcode Submissions/345_Reverse_Vowels_Of_A_String.cpp
@@ -22,3 +22,28 @@ public:
         return s;
     }
 };
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        
+        vector<char> v;
+        unordered_set<char> vow = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(vow.find(s[i]) != vow.end())
+                v.push_back(s[i]);
+        }
+        reverse(v.begin(), v.end());
+        int rev = 0;
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(vow.find(s[i]) != vow.end()){
+                s[i] = v[rev];
+                rev++;
+            }
+        }
+        return s;
+    }
+};
