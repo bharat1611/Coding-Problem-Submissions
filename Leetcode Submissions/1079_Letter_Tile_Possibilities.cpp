1f2ec8d7b139e7e0b3be488779c9f4465bd712b2
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-03-10 15:00:31 +0530

    1079_Letter_Tile_Possibilities- Solution 2

--- Leetcode Submissions/1079_Letter_Tile_Possibilities.cpp
+++ Leetcode Submissions/1079_Letter_Tile_Possibilities.cpp
@@ -25,3 +25,25 @@ public:
         return sum;
     }
 };
+
+class Solution {
+public:
+    void backtrack(string tiles, int level, int &count)
+    {
+        count++;
+        for(int i = level; i < tiles.size(); i++)
+        {
+            // to avoid repeating characters
+            if(i != level and tiles[i] == tiles[level])
+                continue;
+            swap(tiles[i], tiles[level]);
+            backtrack(tiles, level + 1, count);
+        }
+    }
+    int numTilePossibilities(string tiles) {
+        int count = -1;
+        sort(tiles.begin(), tiles.end());
+        backtrack(tiles, 0, count);
+        return count;
+    }
+};
