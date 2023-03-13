da9790a90c52e281daca2a04f860641cfb8415e4
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-03-13 10:08:44 +0530

    434_Number_Of_SEgments_In_A_String - Solution 2

--- Leetcode Submissions/434_Number_Of_SEgments_In_A_String.cpp
+++ Leetcode Submissions/434_Number_Of_SEgments_In_A_String.cpp
@@ -1,12 +1,31 @@
+// class Solution {
+// public:
+//     int countSegments(string s) {
+//         int count = 0;
+//         stringstream ss(s);
+//         string word;
+//         while(ss >> word)
+//         {
+//             count++;
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int countSegments(string s) {
         int count = 0;
-        stringstream ss(s);
-        string word;
-        while(ss >> word)
+        for(int i = 0 ; i < s.length(); i++)
         {
-            count++;
+            if(s[i] != ' ')
+            {
+                cout << i << " " ;
+                while(i < s.length() and s[i] != ' ')
+                    i++;
+                count++;
+            }
+            
         }
         return count;
     }
