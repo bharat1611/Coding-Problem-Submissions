cfbb5dc158fae18832643e2b667fd709ef06a310
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-09-10 18:34:20 +0530

    Count_Ways_to_Reach_Nth_Stairs

--- Coding Ninjas/Count_Ways_to_Reach_Nth_Stairs.cpp
+++ Coding Ninjas/Count_Ways_to_Reach_Nth_Stairs.cpp
@@ -1,39 +1,39 @@
 // Recursion
-// #include <bits/stdc++.h> 
-// #define MOD 1000000007
-// int solve(int n, int i, vector<int> &dp)
-// {
-//     if(i == n)
-//         return 1;
-//     if(i > n)
-//         return 0;
+#include <bits/stdc++.h> 
+#define MOD 1000000007
+int solve(int n, int i, vector<int> &dp)
+{
+    if(i == n)
+        return 1;
+    if(i > n)
+        return 0;
     
-//     return (solve(n, i + 1, dp) + solve(n, i + 2, dp))%MOD;
+    return (solve(n, i + 1, dp) + solve(n, i + 2, dp))%MOD;
     
-// }
-// int countDistinctWays(int n) {
-//     return solve(n, 0, dp)%MOD;
-// }
+}
+int countDistinctWays(int n) {
+    return solve(n, 0, dp)%MOD;
+}
 
 // Memoisation
-// #include <bits/stdc++.h> 
-// #define MOD 1000000007
-// int solve(int n, int i, vector<int> &dp)
-// {
-//     if(i == n)
-//         return 1;
-//     if(i > n)
-//         return 0;
-//     if(dp[i] != -1)
-//         return dp[i];
+#include <bits/stdc++.h> 
+#define MOD 1000000007
+int solve(int n, int i, vector<int> &dp)
+{
+    if(i == n)
+        return 1;
+    if(i > n)
+        return 0;
+    if(dp[i] != -1)
+        return dp[i];
     
-//     return dp[i] = (solve(n, i + 1, dp) + solve(n, i + 2, dp))%MOD;
+    return dp[i] = (solve(n, i + 1, dp) + solve(n, i + 2, dp))%MOD;
     
-// }
-// int countDistinctWays(int n) {
-//     vector<int> dp(n + 1, -1);
-//     return solve(n, 0, dp)%MOD;
-// }
+}
+int countDistinctWays(int n) {
+    vector<int> dp(n + 1, -1);
+    return solve(n, 0, dp)%MOD;
+}
 
 // Tabulation
 #include <bits/stdc++.h> 
