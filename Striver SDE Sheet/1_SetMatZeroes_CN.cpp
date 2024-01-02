05215f214d5548c4f0a34d4ea8f73fdf3d6b347b
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-02 11:30:10 +0530

    1_SetMatrixZeroes Optimised

--- Striver SDE Sheet/1_SetMatZeroes_CN.cpp
+++ Striver SDE Sheet/1_SetMatZeroes_CN.cpp
@@ -1,22 +1,57 @@
 #include <bits/stdc++.h> 
 vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
 
-	vector<int> row(n, 1);
-	vector<int> col(m, 1);
+	// vector<int> row(n, 1);
+	// vector<int> col(m, 1);
 
-	for(int i = 0; i < n; i++){
-		for(int j = 0 ; j < m; j++){
-			if(matrix[i][j] == 0){
-				row[i] = 0;
-				col[j] = 0;
-			}
-		}
-	}
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
-			if((row[i] == 0) or (col[j] == 0))
+	// for(int i = 0; i < n; i++){
+	// 	for(int j = 0 ; j < m; j++){
+	// 		if(matrix[i][j] == 0){
+	// 			row[i] = 0;
+	// 			col[j] = 0;
+	// 		}
+	// 	}
+	// }
+	// for(int i = 0; i < n; i++){
+	// 	for(int j = 0; j < m; j++){
+	// 		if((row[i] == 0) or (col[j] == 0))
+	// 			matrix[i][j] = 0;
+	// 	}
+	// }
+	// return matrix;
+
+	int col0 = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) 
+			{
+                matrix[i][0] = 0;
+				
+                if (j != 0)
+                    matrix[0][j] = 0;
+                else
+                    col0 = 0;
+            }
+        }
+    }
+
+
+	for(int i = n - 1; i >= 1; i--){
+		for(int j = m - 1; j >= 1; j--){
+			if(!matrix[i][0] || !matrix[0][j])
 				matrix[i][j] = 0;
 		}
 	}
+	if(matrix[0][0] == 0){
+		for(int i = 0 ; i < m; i++){
+			matrix[0][i] = 0;
+		}
+	}	
+
+	if(col0 == 0){
+		for(int i = 0; i < n; i++)
+			matrix[i][0] = 0;
+	}
 	return matrix;
+
 }
