c1e8df98ecd63a00637b31b50309a4ab678b04f6
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-04 20:00:44 +0530

    4_Kadanes Algo updated

--- Striver SDE Sheet/4_KadanesAlgorithm_Leetcode.cpp
+++ Striver SDE Sheet/4_KadanesAlgorithm_Leetcode.cpp
@@ -1,16 +1,18 @@
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//     //     vector<int> dp(nums);
-//     //     int ans = nums[0];
-//     //     for(int i = 1 ; i < nums.size(); i++){
-//     //         dp[i] = max(dp[i], dp[i - 1] + nums[i]);
-//     //         ans = max(ans, dp[i]);
-//     //     }
-//     //     return ans;   
-//     // }
-// };
+//O(N) with O(N) space
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums);
+        int ans = nums[0];
+        for(int i = 1 ; i < nums.size(); i++){
+            dp[i] = max(dp[i], dp[i - 1] + nums[i]);
+            ans = max(ans, dp[i]);
+        }
+        return ans;   
+    }
+};
 
+//O(N) with O(1) space
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -28,3 +30,21 @@ public:
     }
 };
 
+// O(n^2)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int max_sum = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) 
+            {
+                sum += nums[j];
+                max_sum = max(max_sum, sum);
+            }
+        }
+        return max_sum;
+    }
+};
