19902e463e162bda56ae859920a36fdda5ebe749
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-05 14:17:55 +0530

    5_Sort0s1s2s_Coding ninjas

---
+++ Striver SDE Sheet/5_Sort0s1s2s_CodingNinjas.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   int low = 0 , high = n - 1;
+   int mid = 0;
+   while(mid <= high)
+   {
+      if(arr[mid] == 0)
+      {
+         swap(arr[low], arr[mid]);
+         low++;
+         mid++;
+      }
+      else if(arr[mid] == 1)
+         mid++;
+      else if(arr[mid] == 2)
+      {
+         swap(arr[mid], arr[high]);
+         high--;
+      }
+   }
+}
