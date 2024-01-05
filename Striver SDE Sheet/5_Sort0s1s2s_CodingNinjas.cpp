1aee5ced2cdd22051e91511dfcb35d49c2bc678b
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-05 14:25:20 +0530

    5_Sort0s1s2s_Coding Ninjas Optimised

--- Striver SDE Sheet/5_Sort0s1s2s_CodingNinjas.cpp
+++ Striver SDE Sheet/5_Sort0s1s2s_CodingNinjas.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h> 
 void sort012(int *arr, int n)
 {
+   int c0 = 0, c1 = 0, c2 = 0;
+   for(int i = 0 ; i < n; i++){
+      if(arr[i] == 0) c0++;
+      else if(arr[i] == 1) c1++;
+      else c2++;
+   }
+
+   for(int i = 0 ; i < n; i++){
+      if(c0 > 0)
+      {
+         arr[i] = 0;
+         c0--;
+      } else if (c0 <= 0 and c1 > 0) {
+         arr[i] = 1;
+         c1--;
+      } else
+         arr[i] = 2;
+   }
+}
+
+// Dutch National Flag Algo
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
    int low = 0 , high = n - 1;
    int mid = 0;
    while(mid <= high)
