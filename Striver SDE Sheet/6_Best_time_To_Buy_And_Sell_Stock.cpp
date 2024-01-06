e735cea1bd4140d78a779d934510d83ca8ced502
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-06 19:36:30 +0530

    6_Best_time_To_Buy_And_Sell_Stock Leetcode

---
+++ Striver SDE Sheet/6_Best_time_To_Buy_And_Sell_Stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = INT_MAX, max_profit = 0;
+        
+        for(int i = 0 ; i < prices.size(); i++)
+        {
+            min_price = min(min_price, prices[i]);
+            if(max_profit < prices[i] - min_price)
+            {
+                max_profit = prices[i] - min_price;
+            }
+        }
+        return max_profit;
+    }
+};
