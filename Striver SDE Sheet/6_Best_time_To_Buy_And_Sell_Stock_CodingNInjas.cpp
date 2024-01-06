97d5ceaf3c2977076a57b9d0daacb64816d190a5
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-06 19:37:38 +0530

    6_Best_time_To_Buy_And_Sell_Stock_CodingNInjas

---
+++ Striver SDE Sheet/6_Best_time_To_Buy_And_Sell_Stock_CodingNInjas.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+        int min_price = INT_MAX, max_profit = 0;
+        
+        for(int i = 0 ; i < prices.size(); i++)
+        {
+            min_price = min(min_price, prices[i]);
+            if(max_profit < prices[i] - min_price)
+            {
+                max_profit = prices[i] - min_price;
+            }
+        }
+        return max_profit;
+    
+}
