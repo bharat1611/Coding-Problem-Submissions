b06b09272bfe6beb2d0db36204c55736b4881b58
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-10 22:00:54 +0530

    10_Find_Duplicate_Element_In_N+1_Integers_CodingNinjas Optimised

--- Striver SDE Sheet/10_Find_Duplicate_Element_In_N+1_Integers_CodingNinjas.cpp
+++ Striver SDE Sheet/10_Find_Duplicate_Element_In_N+1_Integers_CodingNinjas.cpp
@@ -1,10 +1,26 @@
+// #include <bits/stdc++.h>
+
+// int findDuplicate(vector<int> &arr, int n){
+// 	sort(arr.begin(), arr.end());
+
+// 	for(int i = 0 ; i < n - 1; i++){
+// 		if(arr[i] == arr[i + 1])
+// 			return arr[i];
+// 	}
+// 	return arr[0];
+// }
+
 #include <bits/stdc++.h>
 
 int findDuplicate(vector<int> &arr, int n){
-	sort(arr.begin(), arr.end());
+	vector<int> freq(n + 1, 0);
 
-	for(int i = 0 ; i < n - 1; i++){
-		if(arr[i] == arr[i + 1])
+	for(int i = 0 ; i < n; i++)
+	{
+		if(freq[arr[i]] == 0){
+			freq[arr[i]]++;
+		}
+		else
 			return arr[i];
 	}
 	return arr[0];
