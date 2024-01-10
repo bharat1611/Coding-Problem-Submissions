46ea678af97f977ef1301a3aafbdc45392730b4d
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-10 21:57:01 +0530

    10_Find_Duplicate_Element_In_N+1_Integers_Leetcode Optimised

--- Striver SDE Sheet/10_Find_Duplicate_Element_In_N+1_Integers.cpp
+++ Striver SDE Sheet/10_Find_Duplicate_Element_In_N+1_Integers.cpp
@@ -1,10 +1,25 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         for(int i = 0 ; i < nums.size() - 1;i++)
+//         {
+//             if(nums[i] == nums[i + 1])
+//                 return nums[i];
+//         }
+//         return nums[0];
+//     }
+// };
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 0 ; i < nums.size() - 1;i++)
-        {
-            if(nums[i] == nums[i + 1])
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
+        for(int i = 0 ; i < nums.size(); i++){
+            if(freq[nums[i]] == 0)
+                freq[nums[i]]++;
+            else
                 return nums[i];
         }
         return nums[0];
