0185da93e95748d1481e36830515aa502dc9a78e
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-11 13:22:37 +0530

    11_Find_The_Missing_And_Repeating_Numbers_coding Ninjas Optimised

--- Striver SDE Sheet/11_Find_The_Missing_And_Repeating_Numbers.cpp
+++ Striver SDE Sheet/11_Find_The_Missing_And_Repeating_Numbers.cpp
@@ -1,18 +1,38 @@
+// #include <bits/stdc++.h>
+
+// pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+// {
+// 	sort(arr.begin(), arr.end());
+// 	int a = 0, b = 0;
+// 	int sum = 0;
+// 	for(int i = 0 ; i <n; i++)
+// 	{
+// 		if(arr[i] == arr[i + 1])
+// 			a = arr[i];
+// 		sum += arr[i];
+// 	}
+// 	sum -= a;
+
+// 	b = (n*(n+1)/2) - sum;
+// 	return { b, a };
+// }
+
 #include <bits/stdc++.h>
 
 pair<int,int> missingAndRepeating(vector<int> &arr, int n)
 {
-	sort(arr.begin(), arr.end());
+	vector<int>v(n + 1, 0);
 	int a = 0, b = 0;
-	int sum = 0;
-	for(int i = 0 ; i <n; i++)
+	for(int i = 0 ; i <= n; i++)
 	{
-		if(arr[i] == arr[i + 1])
-			a = arr[i];
-		sum += arr[i];
+		v[arr[i]]++;
+	}	
+	for(int i = 0 ; i < v.size(); i++)
+	{
+		if(v[i] == 0)
+			a = i;
+		if(v[arr[i]] == 2)
+			b = arr[i];
 	}
-	sum -= a;
-
-	b = (n*(n+1)/2) - sum;
-	return { b, a };
+        return { a,b };
 }
