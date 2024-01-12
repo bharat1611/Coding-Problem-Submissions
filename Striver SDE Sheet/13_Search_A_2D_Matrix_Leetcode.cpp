72b4564b5a6b66882799e618332b396a6ac8eb2e
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-12 21:42:14 +0530

    13_Search_A_2D_Matrix_Leetcode

---
+++ Striver SDE Sheet/13_Search_A_2D_Matrix_Leetcode.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(); 
+        int n = matrix[0].size();
+        int i = 0, j = n - 1;
+        
+        while(i >= 0 and j >= 0 and i < m and j < n)
+        {
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target)
+                j--;
+            else if(matrix[i][j] < target)
+                i++;
+        }
+        return false;
+        
+    }
+};
