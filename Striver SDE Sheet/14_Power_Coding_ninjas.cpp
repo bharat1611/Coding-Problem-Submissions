123fb23b0307b124fd94fc2c6bba8d4f910da429
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-13 21:15:33 +0530

    14_Power_Coding_ninjas

---
+++ Striver SDE Sheet/14_Power_Coding_ninjas.cpp
@@ -0,0 +1,10 @@
+double myPow(double x, int n) {
+    
+    double ans = 1;
+    for(int i = 0 ; i < abs(n); i++){
+        ans = ans * x;
+    }
+    if(x < 0 or n < 0)
+        return 1/ans;
+    return ans;
+}
