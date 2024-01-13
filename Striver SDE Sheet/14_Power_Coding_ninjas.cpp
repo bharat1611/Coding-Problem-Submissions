eb45a6cd8e5aa7f8105b0df45848ba41515ced76
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-13 21:17:48 +0530

    14_Power_Coding_ninjas Sol 2

--- Striver SDE Sheet/14_Power_Coding_ninjas.cpp
+++ Striver SDE Sheet/14_Power_Coding_ninjas.cpp
@@ -1,10 +1,14 @@
-double myPow(double x, int n) {
+// double myPow(double x, int n) {
     
-    double ans = 1;
-    for(int i = 0 ; i < abs(n); i++){
-        ans = ans * x;
-    }
-    if(x < 0 or n < 0)
-        return 1/ans;
-    return ans;
+//     double ans = 1;
+//     for(int i = 0 ; i < abs(n); i++){
+//         ans = ans * x;
+//     }
+//     if(x < 0 or n < 0)
+//         return 1/ans;
+//     return ans;
+// }
+
+double myPow(double x, int n) {
+    return pow(x,n);
 }
