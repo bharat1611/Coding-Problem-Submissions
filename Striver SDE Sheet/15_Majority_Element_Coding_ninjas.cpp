e81467970e3af0908f2b6b2df7d9e2801e29e7a8
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-14 20:53:58 +0530

    15_Majority_Element Coding Ninjas Optimised

--- Striver SDE Sheet/15_Majority_Element_Coding_ninjas.cpp
+++ Striver SDE Sheet/15_Majority_Element_Coding_ninjas.cpp
@@ -1,11 +1,30 @@
-#include <unordered_map>
+// #include <unordered_map>
+// int majorityElement(vector<int> v) {
+// 	int n = v.size();
+// 	unordered_map<int, int> mp;
+// 	for(int i = 0 ;i < n; i++){
+// 		mp[v[i]]++;
+// 		if(mp[v[i]] > n/2)
+// 			return v[i];
+// 	}
+// 	return v[0];
+// }
+
+
 int majorityElement(vector<int> v) {
 	int n = v.size();
-	unordered_map<int, int> mp;
-	for(int i = 0 ;i < n; i++){
-		mp[v[i]]++;
-		if(mp[v[i]] > n/2)
-			return v[i];
+	int element = 0, count = 0;
+	for(int i = 0 ; i < n; i++)
+	{
+		if(count == 0){
+			count++;
+			element = v[i];
+		}
+		else if(element != v[i]){
+			count--;
+		}
+		else if(element == v[i])
+			count++;
 	}
-	return v[0];
+	return element;
 }
