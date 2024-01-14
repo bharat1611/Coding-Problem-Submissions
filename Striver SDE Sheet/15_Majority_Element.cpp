49c3b70fcc30c5967de517c56c040d1f2c698028
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-14 20:42:04 +0530

    15_Majority_Element Leetcode Optimised

--- Striver SDE Sheet/15_Majority_Element.cpp
+++ Striver SDE Sheet/15_Majority_Element.cpp
@@ -1,14 +1,34 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int, int> mp;
+//         for(int i = 0 ; i < n; i++)
+//         {
+//             mp[nums[i]]++;
+//             if(mp[nums[i]] > n/2)
+//                 return nums[i];
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> mp;
+        int cnt = 0, ele = 0;
         for(int i = 0 ; i < n; i++)
         {
-            mp[nums[i]]++;
-            if(mp[nums[i]] > n/2)
-                return nums[i];
+            if(cnt == 0){
+                ele = nums[i];
+                cnt++;
+            }
+            else if(ele != nums[i])
+                cnt--;
+            else if(ele == nums[i])
+                cnt++;
         }
-        return -1;
+        return ele;
     }
 };
