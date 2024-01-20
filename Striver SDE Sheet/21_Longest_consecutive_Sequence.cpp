ff6789fbb440db7c4e188677d70da3377212dfca
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-01-20 21:52:34 +0530

    21_Longest_consecutive_Sequence Optimised

--- Striver SDE Sheet/21_Longest_consecutive_Sequence.cpp
+++ Striver SDE Sheet/21_Longest_consecutive_Sequence.cpp
@@ -1,20 +1,45 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
+// int lengthOfLongestConsecutiveSequence(vector<int> &nums, int n) {
+//     sort(nums.begin(), nums.end());
+//     int ans = 1, curr = 1;
+//     for(int i = 0 ; i < nums.size() - 1; i++){
+//         if(nums[i] == nums[i + 1]){
+//             continue;
+//         }
+//         else{
+//             if (nums[i] == nums[i + 1] - 1) {
+//               curr++;
+//             } else {
+//               ans = max(ans, curr);
+//               curr = 1;
+//             }
+//         }
+//     }
+//     return max(ans, curr);
+// }
+
+#include <bits/stdc++.h>
 int lengthOfLongestConsecutiveSequence(vector<int> &nums, int n) {
-    sort(nums.begin(), nums.end());
-    int ans = 1, curr = 1;
-    for(int i = 0 ; i < nums.size() - 1; i++){
-        if(nums[i] == nums[i + 1]){
+    unordered_map<int, int> mp;
+    for(auto &i: nums)
+        mp[i]++;
+    int ans = 1, count = 1;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(mp.find(nums[i] - 1) != mp.end()){
             continue;
         }
-        else{
-            if (nums[i] == nums[i + 1] - 1) {
-              curr++;
-            } else {
-              ans = max(ans, curr);
-              curr = 1;
+        else
+        {
+            int k = nums[i];
+            while(mp.find(k + 1) != mp.end()){
+                k++;
+                count++;
             }
+            ans = max(count, ans);
+            count = 1;
         }
     }
-    return max(ans, curr);
+    return ans;
 }
