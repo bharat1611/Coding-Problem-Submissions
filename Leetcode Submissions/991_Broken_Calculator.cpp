11cbbfe1d2e9c9a1306fdc9532c96759d32531c7
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-02-16 19:49:48 +0530

    991_Broken_Calculator

---
+++ Leetcode Submissions/991_Broken_Calculator.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        // we work backwards, to convert target to startValue
+        int count = 0;
+        while(target != startValue){
+            
+            if(target < startValue)
+            {
+                target++;
+            }
+            else if(target > startValue){
+                if(target%2 == 0){
+                    target = target/2;
+                }
+                else
+                    target++;
+            }
+            count++;
+        }
+        return count;
+    }
+};
