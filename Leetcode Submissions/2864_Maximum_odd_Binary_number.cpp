33db415f04eca21243f951960af904eac8d2fb86
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-03-02 20:32:23 +0530

    2864_Maximum_odd_Binary_number

---
+++ Leetcode Submissions/2864_Maximum_odd_Binary_number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        
+        sort(s.rbegin(), s.rend());
+        int n = s.length();
+        
+        for(int i = n - 1; i >= 0; i--){
+            if(s[i] == '1')
+            {
+                swap(s[i], s[n-1]);
+                break;
+            }
+        }
+        return s;
+    }
+};
