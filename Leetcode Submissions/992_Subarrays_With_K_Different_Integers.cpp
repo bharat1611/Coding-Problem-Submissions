0e47fd84e228eadd9105bee97cbe918c71d9f136
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-03-30 20:35:01 +0530

    992_Subarrays_With_K_Different_Integers

---
+++ Leetcode Submissions/992_Subarrays_With_K_Different_Integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int func(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        int start = 0;
+        int count = 0;
+        for(int end = 0; end < nums.size(); end++)
+        { 
+            mp[nums[end]]++;
+            while(mp.size() > k)
+            {
+                mp[nums[start]]--;
+                if(mp[nums[start]] == 0)
+                    mp.erase(nums[start]);
+                start++;
+            }
+            count += (end - start + 1);
+        }
+        return count;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return func(nums, k) - func(nums, k - 1);
+    }
+};
